aaebae81146d8ab1227e4584c40056571287c5eb
Jon Cram <webignition@gmail.com>
2019-11-21 12:04:47 +0000

    Parse step data (#12)

---
+++ src/StepParser.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilParser;
+
+use webignition\BasilDataStructure\Action\ActionInterface;
+use webignition\BasilDataStructure\AssertionInterface;
+use webignition\BasilDataStructure\Step;
+
+class StepParser
+{
+    private const KEY_ACTIONS = 'actions';
+    private const KEY_ASSERTIONS = 'assertions';
+    private const KEY_IMPORT_NAME = 'use';
+    private const KEY_DATA = 'data';
+    private const KEY_ELEMENTS = 'elements';
+
+    private $actionParser;
+    private $assertionParser;
+
+    public function __construct(ActionParser $actionParser, AssertionParser $assertionParser)
+    {
+        $this->actionParser = $actionParser;
+        $this->assertionParser = $assertionParser;
+    }
+
+    public static function create(): StepParser
+    {
+        return new StepParser(
+            ActionParser::create(),
+            AssertionParser::create()
+        );
+    }
+
+    public function parse(array $stepData): Step
+    {
+        $actions = $this->parseActions($stepData[self::KEY_ACTIONS] ?? []);
+        $assertions = $this->parseAssertions($stepData[self::KEY_ASSERTIONS] ?? []);
+
+        $step = new Step($actions, $assertions);
+        $step = $this->setImportName($step, $stepData[self::KEY_IMPORT_NAME] ?? null);
+        $step = $this->setData($step, $stepData[self::KEY_DATA] ?? null);
+        $step = $this->setElements($step, $stepData[self::KEY_ELEMENTS] ?? null);
+
+
+        return $step;
+    }
+
+    /**
+     * @param array $actionsData
+     *
+     * @return ActionInterface[]
+     */
+    private function parseActions(array $actionsData): array
+    {
+        $actions = [];
+
+        foreach ($actionsData as $actionString) {
+            if (is_string($actionString)) {
+                $actions[] = $this->actionParser->parse($actionString);
+            }
+        }
+
+        return $actions;
+    }
+
+    /**
+     * @param array $assertionsData
+     *
+     * @return AssertionInterface[]
+     */
+    private function parseAssertions(array $assertionsData): array
+    {
+        $assertions = [];
+
+        foreach ($assertionsData as $assertionString) {
+            if (is_string($assertionString)) {
+                $assertions[] = $this->assertionParser->parse($assertionString);
+            }
+        }
+
+        return $assertions;
+    }
+
+    private function setImportName(Step $step, $importName): Step
+    {
+        if (!is_string($importName)) {
+            $importName = null;
+        }
+
+        if (is_string($importName)) {
+            $step = $step->withImportName($importName);
+        }
+
+        return $step;
+    }
+
+    private function setData(Step $step, $data): Step
+    {
+        if (is_array($data)) {
+            $step = $step->withDataArray($data);
+        }
+
+        if (is_string($data)) {
+            $step = $step->withDataImportName($data);
+        }
+
+        return $step;
+    }
+
+    private function setElements(Step $step, $elements): Step
+    {
+        if (is_array($elements)) {
+            $step = $step->withElements($elements);
+        }
+
+        return $step;
+    }
+}
---
+++ tests/Unit/StepParserTest.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilParser\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use webignition\BasilDataStructure\Action\InteractionAction;
+use webignition\BasilDataStructure\Assertion;
+use webignition\BasilDataStructure\Step;
+use webignition\BasilParser\StepParser;
+
+class StepParserTest extends TestCase
+{
+    /**
+     * @dataProvider parseDataProvider
+     */
+    public function testParse(array $stepData, Step $expectedStep)
+    {
+        $parser = StepParser::create();
+
+        $this->assertEquals($expectedStep, $parser->parse($stepData));
+    }
+
+    public function parseDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'stepData' => [],
+                'expectedStep' => new Step([], []),
+            ],
+            'single action' => [
+                'stepData' => [
+                    'actions' => [
+                        'click ".selector"',
+                    ],
+                ],
+                'expectedStep' => new Step(
+                    [
+                        new InteractionAction(
+                            'click ".selector"',
+                            'click',
+                            '".selector"',
+                            '".selector"'
+                        )
+                    ],
+                    []
+                ),
+            ],
+            'single assertion' => [
+                'stepData' => [
+                    'assertions' => [
+                        '".selector" exists',
+                    ],
+                ],
+                'expectedStep' => new Step(
+                    [],
+                    [
+                        new Assertion('".selector" exists', '".selector"', 'exists')
+                    ]
+                ),
+            ],
+            'invalid import name; not a string' => [
+                'stepData' => [
+                    'use' => true,
+                ],
+                'expectedStep' => new Step([], []),
+            ],
+            'valid import name' => [
+                'stepData' => [
+                    'use' => 'import_name',
+                ],
+                'expectedStep' => (new Step([], []))->withImportName('import_name'),
+            ],
+            'invalid data import name; not a string' => [
+                'stepData' => [
+                    'data' => true,
+                ],
+                'expectedStep' => new Step([], []),
+            ],
+            'valid data import name' => [
+                'stepData' => [
+                    'data' => 'data_import_name',
+                ],
+                'expectedStep' => (new Step([], []))->withDataImportName('data_import_name'),
+            ],
+            'valid data array' => [
+                'stepData' => [
+                    'data' => [
+                        'set1' => [
+                            'key' => 'value',
+                        ],
+                    ],
+                ],
+                'expectedStep' => (new Step([], []))->withDataArray([
+                    'set1' => [
+                        'key' => 'value',
+                    ],
+                ]),
+            ],
+            'invalid elements; not an array' => [
+                'stepData' => [
+                    'elements' => 'string',
+                ],
+                'expectedStep' => new Step([], []),
+            ],
+            'valid elements' => [
+                'stepData' => [
+                    'elements' => [
+                        'heading' => 'page_import_name.elements.heading',
+                    ],
+                ],
+                'expectedStep' => (new Step([], []))->withElements([
+                    'heading' => 'page_import_name.elements.heading',
+                ]),
+            ],
+        ];
+    }
+}
