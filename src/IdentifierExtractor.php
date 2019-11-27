2f9aa7e95a8c855edcd007f532fe77671c099859
Jon Cram <jon@webignition.net>
2019-11-27 11:10:11 +0000

    Update action parsing

--- src/IdentifierExtractor.php
+++ src/IdentifierExtractor.php
@@ -4,22 +4,18 @@ declare(strict_types=1);
 
 namespace webignition\BasilParser;
 
-use webignition\BasilParser\ValueExtractor\LiteralValueExtractor;
 use webignition\BasilParser\ValueExtractor\PageElementIdentifierExtractor;
 use webignition\BasilParser\ValueExtractor\VariableValueExtractor;
 
 class IdentifierExtractor
 {
-    private $literalValueExtractor;
     private $pageElementIdentifierExtractor;
     private $variableValueExtractor;
 
     public function __construct(
-        LiteralValueExtractor $literalValueExtractor,
         PageElementIdentifierExtractor $pageElementIdentifierExtractor,
         VariableValueExtractor $variableValueExtractor
     ) {
-        $this->literalValueExtractor = $literalValueExtractor;
         $this->pageElementIdentifierExtractor = $pageElementIdentifierExtractor;
         $this->variableValueExtractor = $variableValueExtractor;
     }
@@ -27,7 +23,6 @@ class IdentifierExtractor
     public static function create(): IdentifierExtractor
     {
         return new IdentifierExtractor(
-            new LiteralValueExtractor(),
             new PageElementIdentifierExtractor(),
             new VariableValueExtractor()
         );
@@ -35,10 +30,6 @@ class IdentifierExtractor
 
     public function extract(string $string): string
     {
-        if ($this->literalValueExtractor->handles($string)) {
-            return $this->literalValueExtractor->extract($string);
-        }
-
         if ($this->pageElementIdentifierExtractor->handles($string)) {
             return $this->pageElementIdentifierExtractor->extract($string);
         }
