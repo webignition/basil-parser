db0920a8d212ee0b0f4d667fe4f0046dafe08274
Jon Cram <webignition@gmail.com>
2019-12-06 11:51:08 +0000

    Add import name to page (#41)

--- tests/Unit/PageParserTest.php
+++ tests/Unit/PageParserTest.php
@@ -14,45 +14,50 @@ class PageParserTest extends TestCase
     /**
      * @dataProvider parseDataProvider
      */
-    public function testParse(array $pageData, PageInterface $expectedPage)
+    public function testParse(string $importName, array $pageData, PageInterface $expectedPage)
     {
         $parser = PageParser::create();
 
-        $this->assertEquals($expectedPage, $parser->parse($pageData));
+        $this->assertEquals($expectedPage, $parser->parse($importName, $pageData));
     }
 
     public function parseDataProvider(): array
     {
         return [
             'empty' => [
+                'importName' => '',
                 'pageData' => [],
-                'expectedPage' => new Page(''),
+                'expectedPage' => new Page('', ''),
             ],
             'invalid url; not a string' => [
+                'importName' => 'import_name',
                 'pageData' => [
                     'url' => true,
                 ],
-                'expectedPage' => new Page(''),
+                'expectedPage' => new Page('import_name', ''),
             ],
             'valid url' => [
+                'importName' => 'import_name',
                 'pageData' => [
                     'url' => 'http://example.com/',
                 ],
-                'expectedPage' => new Page('http://example.com/'),
+                'expectedPage' => new Page('import_name', 'http://example.com/'),
             ],
             'invalid elements; not an array' => [
+                'importName' => '',
                 'pageData' => [
                     'elements' => 'string',
                 ],
-                'expectedPage' => new Page('', []),
+                'expectedPage' => new Page('', '', []),
             ],
             'valid elements' => [
+                'importName' => '',
                 'pageData' => [
                     'elements' => [
                         'heading' => 'page_import_name.elements.heading',
                     ],
                 ],
-                'expectedPage' => new Page('', [
+                'expectedPage' => new Page('', '', [
                     'heading' => 'page_import_name.elements.heading',
                 ]),
             ],
