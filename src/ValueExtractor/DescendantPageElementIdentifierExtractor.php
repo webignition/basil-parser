762515d5c0587c8ce738f1eef1ae139ff7357672
Jon Cram <jon@webignition.net>
2019-12-19 15:17:18 +0000

    Extract identifiers containing nested parents

--- src/ValueExtractor/DescendantPageElementIdentifierExtractor.php
+++ src/ValueExtractor/DescendantPageElementIdentifierExtractor.php
@@ -5,7 +5,8 @@ namespace webignition\BasilParser\ValueExtractor;
 class DescendantPageElementIdentifierExtractor
 {
     private const PARENT_PREFIX = '{{ ';
-    private const PARENT_SUFFIX = ' }} ';
+    private const PARENT_SUFFIX = ' }}';
+    private const PARENT_MATCH_LENGTH = 3;
 
     private $pageElementIdentifierExtractor;
 
@@ -27,29 +28,79 @@ class DescendantPageElementIdentifierExtractor
             return '';
         }
 
-        $parentSuffixPosition = mb_strpos($string, self::PARENT_SUFFIX);
-
-        if (false === $parentSuffixPosition) {
+        $parentSuffixPosition = $this->findParentSuffixPosition($string);
+        if (null === $parentSuffixPosition) {
             return '';
         }
 
         $parentReference = mb_substr($string, 0, $parentSuffixPosition + strlen(self::PARENT_SUFFIX));
-        if (false === $this->isParentReference($parentReference)) {
+        $parentReferenceIdentifier = $this->unwrap($parentReference);
+
+        if (false === $this->isParentReference($parentReferenceIdentifier)) {
             return '';
         }
 
-        $childReferencePart = mb_substr($string, mb_strlen($parentReference));
-
+        $childReferencePart = mb_substr($string, mb_strlen($parentReference) + 1);
         $childReference = $this->pageElementIdentifierExtractor->extract($childReferencePart);
+
         if ('' === $childReference) {
             return '';
         }
 
-        return $parentReference . $childReference;
+        return $parentReference . ' ' . $childReference;
     }
 
     private function isParentReference(string $string): bool
     {
-        return '' !== $this->pageElementIdentifierExtractor->extract(trim($string, '{} '));
+        if ('' !== $this->extract($string)) {
+            return true;
+        }
+
+        if ('' !== $this->pageElementIdentifierExtractor->extract($string)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function findParentSuffixPosition(string $string): ?int
+    {
+        $position = null;
+        $depth = 0;
+        $characters = preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY);
+        if (false === $characters) {
+            return null;
+        }
+
+        $previousCharacters = implode('', array_slice($characters, 0, self::PARENT_MATCH_LENGTH));
+        $characters = array_slice($characters, self::PARENT_MATCH_LENGTH);
+
+        foreach ($characters as $index => $character) {
+            if (self::PARENT_PREFIX === $previousCharacters) {
+                $depth++;
+            }
+
+            if (self::PARENT_SUFFIX === $previousCharacters) {
+                $depth--;
+            }
+
+            if ($depth === 0) {
+                return $index;
+            }
+
+            $previousCharacters .= $character;
+            $previousCharacters = mb_substr($previousCharacters, 1);
+        }
+
+        return null;
+    }
+
+    private function unwrap(string $wrappedIdentifier): string
+    {
+        return mb_substr(
+            $wrappedIdentifier,
+            self::PARENT_MATCH_LENGTH,
+            mb_strlen($wrappedIdentifier) - self::PARENT_MATCH_LENGTH - self::PARENT_MATCH_LENGTH
+        );
     }
 }
--- tests/DataProvider/DescendantPageElementIdentifierStringDataProviderTrait.php
+++ tests/DataProvider/DescendantPageElementIdentifierStringDataProviderTrait.php
@@ -11,9 +11,22 @@ trait DescendantPageElementIdentifierStringDataProviderTrait
                 'string' => '{{ $".parent" }} $".child"',
                 'expectedIdentifierString' => '{{ $".parent" }} $".child"',
             ],
+            'interaction action arguments with indirect parent' => [
+                'string' => '{{ {{ $".inner-parent" }} $".inner-child" }} $".child"',
+                'expectedIdentifierString' => '{{ {{ $".inner-parent" }} $".inner-child" }} $".child"',
+            ],
+            'interaction action arguments with indirectly indirect parent' => [
+                'string' => '{{ {{ {{ $".inner-inner-parent" }} $".inner-inner-child" }} $".inner-child" }} $".child"',
+                'expectedIdentifierString' =>
+                    '{{ {{ {{ $".inner-inner-parent" }} $".inner-inner-child" }} $".inner-child" }} $".child"',
+            ],
             'input action arguments' => [
                 'string' => '{{ $".parent" }} $".child" to "value"',
                 'expectedIdentifierString' => '{{ $".parent" }} $".child"',
+            ],
+            'input action arguments with indirect parent' => [
+                'string' => '{{ {{ $".inner-parent" }} $".inner-child" }} $".child" to "value"',
+                'expectedIdentifierString' => '{{ {{ $".inner-parent" }} $".inner-child" }} $".child"',
             ],
         ];
     }
